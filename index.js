5c233eab1a0d7aa858e1a4be7ff8c11faf7bcdd9
Varshithrangapuram <110088029+Varshithrangapuram@users.noreply.github.com>
2024-10-24 06:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const authRoutes = require('./routes/auth'); // Import auth routes
 const roomRoutes = require('./routes/rooms'); // Import room routes
 const cors = require('cors');
 
+const path = require('path');
+
 
 const app = express();
 app.use(express.json());
@@ -23,10 +25,14 @@ mongoose.connect(mongoURI)
 app.use('/auth', authRoutes); // Prefix for authentication routes
 app.use('/rooms', roomRoutes); // Prefix for room-related routes
 
-// Sample route
-app.get("/", (req, res) => {
-    res.send("This is the server side");
-});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+
 
 // Start the server
 const PORT = 8000;
